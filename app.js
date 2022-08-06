004175394fdd2d4f5c64be6513fbea93037e730f
ZuandaBaransyahPutra <zuandabaransyahputra@gmail.com>
2022-08-06 14:44:41 +0700

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
